b92165a67a41556abbbc5477210b74f511905b53
Steven Haryanto <stevenharyanto@gmail.com>
2010-02-27 03:17:31 +0000

    Import of SHARYANTO/Log-Dispatch-Dir-0.04 from CPAN.
    gitpan-cpan-distribution: Log-Dispatch-Dir
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SHARYANTO/Log-Dispatch-Dir-0.04.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Log/Dispatch/Dir.pm
+++ lib/Log/Dispatch/Dir.pm
@@ -1,5 +1,5 @@
 package Log::Dispatch::Dir;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 # ABSTRACT: Log messages to separate files in a directory, with rotate options
 
 
@@ -187,7 +187,7 @@ Log::Dispatch::Dir - Log messages to separate files in a directory, with rotate 
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 =head1 SYNOPSIS
 
