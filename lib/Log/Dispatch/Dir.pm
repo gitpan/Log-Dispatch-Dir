3851000da8164881c6e9dd377f65ae4608a6f116
Steven Haryanto <stevenharyanto@gmail.com>
2014-05-17 14:00:31 +0000

    Import of SHARYANTO/Log-Dispatch-Dir-0.12 from CPAN.
    gitpan-cpan-distribution: Log-Dispatch-Dir
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         SHARYANTO/Log-Dispatch-Dir-0.12.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Log/Dispatch/Dir.pm
+++ lib/Log/Dispatch/Dir.pm
@@ -6,13 +6,13 @@ use strict;
 use Log::Dispatch::Output;
 use base qw(Log::Dispatch::Output);
 
-use File::Slurp;
+use File::Slurp::Tiny qw(write_file);
 #use File::Stat qw(:stat); # doesn't work in all platforms?
 use Params::Validate qw(validate SCALAR CODEREF);
 use POSIX;
 use Taint::Util;
 
-our $VERSION = '0.11'; # VERSION
+our $VERSION = '0.12'; # VERSION
 
 Params::Validate::validation_options( allow_extra => 1 );
 
@@ -210,13 +210,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Log::Dispatch::Dir - Log messages to separate files in a directory, with rotate options
 
 =head1 VERSION
 
-version 0.11
+This document describes version 0.12 of Log::Dispatch::Dir (from Perl distribution Log-Dispatch-Dir), released on 2014-05-17.
 
 =head1 SYNOPSIS
 
@@ -403,13 +405,29 @@ Log::Dispatch::Output).
 
 L<Log::Dispatch>
 
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Log-Dispatch-Dir>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Log-Dispatch-Dir>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Log-Dispatch-Dir>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =head1 AUTHOR
 
 Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
